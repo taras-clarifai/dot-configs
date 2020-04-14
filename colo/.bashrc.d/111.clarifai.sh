0650ead18660b1ef1a939bf1b60834d5f88d0ca1
Taras Kushnir <taras.kushnir@clarifai.com>
2020-04-14 04:42:42 -0400

    Improve clarifai source

--- colo/.bashrc.d/111.clarifai.sh
+++ colo/.bashrc.d/111.clarifai.sh
@@ -1,4 +1,4 @@
-if [ -e ~/work/clarifai/scripts/amazon/config/ubuntu/.bashrc ];
+if [ -e $HOME/work/clarifai/scripts/amazon/config/ubuntu/.bashrc ];
 then
-  source ~/work/clarifai/scripts/amazon/config/ubuntu/.bashrc
+  source $HOME/work/clarifai/scripts/amazon/config/ubuntu/.bashrc
 fi

9a5fad5824f14a5fe257139480c23ca422c68942
Taras Kushnir <taras.kushnir@clarifai.com>
2020-04-14 04:56:40 -0400

    Add config setup script

---
+++ setup_colo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $HOME
+
+TS=$(date +"%Y-%m-%d-%T")
+BACKUP_DIR="${HOME}/.bashrc_backups/${TS}"
+mkdir -p "${BACKUP_DIR}"
+
+echo "Backup up existing .bashrc to ${BACKUP_DIR}..."
+cp $HOME/.bashrc "${BACKUP_DIR}/"
+
+REPO_ROOT="$HOME/dot-configs"
+cp -v "${REPO_ROOT}/colo/.bashrc" $HOME/
+cp -v -r "${REPO_ROOT}/colo/.bashrc.d/" $HOME/
+
+echo "Done."

c63d00769a3512c389de9bcceda9d250ca53c144
Taras Kushnir <taras.kushnir@clarifai.com>
2020-04-14 04:38:29 -0400

    Add colo setup scripts

---
+++ colo/.bashrc
@@ -0,0 +1,4 @@
+for FN in $HOME/.bashrc.d/*.sh ; do
+    source "$FN"
+done
+
---
+++ colo/.bashrc.d/111.clarifai.sh
@@ -0,0 +1,4 @@
+if [ -e ~/work/clarifai/scripts/amazon/config/ubuntu/.bashrc ];
+then
+  source ~/work/clarifai/scripts/amazon/config/ubuntu/.bashrc
+fi

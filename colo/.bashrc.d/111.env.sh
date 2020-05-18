ddd7ccae2c2f2629ff9705604a19f15db5f5c021
Taras Kushnir <taras.kushnir@clarifai.com>
2020-05-18 13:18:05 +0300

    Another fix for vim

--- colo/.bashrc.d/111.env.sh
+++ colo/.bashrc.d/111.env.sh
@@ -3,7 +3,7 @@ alias vib="vim -u NONE"
 export EDITOR="vim -u NONE"
 export MYVIMRC='$HOME/.vim8rc'
 
-alias myvim=VIMINIT='source $MYVIMRC' vim8
+alias myvim="VIMINIT='source $MYVIMRC' vim8"
 
 export GOPATH=$CLARIFAI_ROOT/go
 export GO15VENDOREXPERIMENT=1
